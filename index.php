3cbabc24d628f43ab1339b5ae7a34f22a569717c
whyu07 <whydot.id7@gmail.com>
2020-05-17 01:37:03 +0700

    Tambah list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 <body>
     <h1>Hellow world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quam eligendi dignissimos? Delectus, dolorum iure perspiciatis asperiores at hic sequi earum incidunt quibusdam iste, quam placeat sit! Aliquid, nemo. Ad!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
